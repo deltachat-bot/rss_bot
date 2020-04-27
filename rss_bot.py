4151871883c96dd0b23284de6f23c251b346a710
missytake <missytake@systemli.org>
2020-04-27 12:52:46 +0200

    test whether RSS feed is valid

--- rss_bot.py
+++ rss_bot.py
@@ -1,6 +1,5 @@
-#import feedparser
+import feedparser
 from deltabot.hookspec import deltabot_hookimpl
-
 
 version = '0.1.0'
 
@@ -10,6 +9,7 @@ def deltabot_init(bot):
     bot.commands.register(name="/subscribe", func=subscribe)
     bot.commands.register(name="/unsubscribe", func=unsubscribe)
 
+
 def subscribe(command):
     """ Subscribes contact to a given RSS feed.
 
@@ -18,17 +18,30 @@ def subscribe(command):
     """
     contact = command.message.get_sender_contact()
     rss_link = command.payload
-    print(rss_link)
-    print(contact)
-    # add rss_link + contact_id to database
-    # reply with "successfully subscribed to https://delta.chat/feed.xml"
+    feed = feedparser.parse(rss_link)
+    try:
+        db_save(rss_link, contact.addr, feed.modified)
+    except AttributeError:
+        return "No valid RSS feed found at " + rss_link
+    return "Successfully subscribed to " + rss_link + "\n\n"
+
 
 def unsubscribe(command):
+    """ Unsubscribes contact from a given RSS feed.
+
+    To use it, tell the bot the RSS feed link, starting with "/unsubscribe".
+    Example: "/unsubscribe https://delta.chat/feed.xml"
+    """
     pass
 
-def post(command):
+
+def post(filter):
     pass
+
 
 def crawl():
     pass
 
+
+def db_save(url, addr, modified):
+    pass
